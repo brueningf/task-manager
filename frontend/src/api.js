b4fdad9a4ca1ba99b39f908df5b23e8e6f6638ae
FredericBruening <contact@fredesk.com>
2024-12-31 15:28:37 -0500

    add react_app to env vars

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
-const dotenv = require('dotenv');
-dotenv.config();
 
-const API_URL = process.env.API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 const apiClient = axios.create({
   baseURL: `${API_URL}/api`,
